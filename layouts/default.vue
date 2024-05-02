bf3d6bbfbd648d3eff1abb402b8b0ebcfbf79c8c
yjineey <did3706@naver.com>
2024-05-02 22:41:54 +0900

    공통 레이아웃 설정

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header>
+      <div>
+        <!-- <NuxtLogo style="width: 50px; height: 50px" /> -->
+        <h1>Nuxt Shopping</h1>
+      </div>
+    </header>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import NuxtLogo from '@/components/NuxtLogo.vue'
+export default {
+  components: NuxtLogo,
+}
+</script>
+
+<style></style>
